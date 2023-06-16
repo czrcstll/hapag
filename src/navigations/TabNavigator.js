9173f55fe53f33888db55b572b99919433206914
czrcstll <45709759+czrcstll@users.noreply.github.com>
2023-06-17 03:23:39 +0800

    CZAR Changes to be committed: 	modified:   src/navigations/AppStackNavigator.js 	modified:   src/navigations/TabNavigator.js

--- src/navigations/TabNavigator.js
+++ src/navigations/TabNavigator.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import { AntDesign, Ionicons, FontAwesome5, Feather, Entypo} from '@expo/vector-icons';
 
 import HomeScreen from '../screens/home/HomeScreen';
 import MealPlanScreen from '../screens/home/MealPlanScreen';
@@ -13,13 +13,37 @@ const Tab = createBottomTabNavigator();
 const TabNavigator = () => {
     return (
             <Tab.Navigator screenOptions={{
-                headerShown: false
+                headerShown: false,
+                tabBarStyle: {backgroundColor: '#fff',paddingTop: 10},
+                tabBarInactiveTintColor: '#F6A43E',
+                tabBarActiveTintColor: '#50269D',
             }}>
-                <Tab.Screen name = 'Home' component={ HomeScreen }/>
-                <Tab.Screen name = 'Shops' component={ ShopScreen }/>
-                <Tab.Screen name = 'Meal Plan' component={ MealPlanScreen }/>
-                <Tab.Screen name = 'Order' component={ OrderScreen }/>
-                <Tab.Screen name = 'Learn' component={ EducationalHubScreen }/>
+                <Tab.Screen name = 'Home2' component = { HomeScreen } 
+                options = {{tabBarIcon: ({color, size}) => (
+                    <Ionicons name = 'ios-home' color = {color} size = {size}/>
+                ) 
+            }}/>
+                <Tab.Screen name = 'Shops' component={ ShopScreen }
+                options = {{tabBarIcon: ({color, size}) => (
+                    <FontAwesome5 name = 'store-alt' color = {color} size = {size}/>
+                ) 
+            }}/>
+                <Tab.Screen name = 'Meal Plan' component={ MealPlanScreen }
+             options = {{tabBarIcon: ({color, size}) => (
+                    <FontAwesome5 name = 'utensils' color = {color} size = {size}/>
+            ) 
+            }}/>
+                <Tab.Screen name = 'Order' component={ OrderScreen }
+            options = {{tabBarIcon: ({color, size}) => (
+                    <Feather name = 'shopping-bag' color = {color} size = {size}/>
+            ) 
+            }}/>
+                <Tab.Screen name = 'Learn' component={ EducationalHubScreen }
+            options = {{tabBarIcon: ({color, size}) => (
+                    <Entypo name = 'light-bulb' color = {color} size = {size}/>
+            ) 
+            }}/>
+
             </Tab.Navigator>
   )
 }
