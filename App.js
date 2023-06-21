af7e50d83e5c51649c6c2d0a78e60b6219ff7232
royceesguerra <royceesguerra@gmail.com>
2023-06-21 22:03:29 +0800

    app.js database

--- App.js
+++ App.js
@@ -1,8 +1,40 @@
 import AppStackNavigator from './src/navigations/AppStackNavigator';
 import 'react-native-gesture-handler';
 import AuthNavigator from './src/navigations/AuthNavigator';
+import * as db from './src/database/Database';
 
 const App = () => {
+
+  // db.default.dropAllTables()
+  // .then((result) => {
+  //   console.log(result);
+  //   // Database opened or created successfully, perform further operations
+  // })
+  // .catch((error) => {
+  //   console.error(error);
+  //   // Failed to open or create the database, handle the error
+  // });
+
+  // db.default.createTables()
+  // .then((result) => {
+  //   console.log(result);
+  //   // Database opened or created successfully, perform further operations
+  // })
+  // .catch((error) => {
+  //   console.error(error);
+  //   // Failed to open or create the database, handle the error
+  // });
+
+  // db.default.getAllTables()
+  // .then((result) => {
+  //   console.log(result);
+  //   // Database opened or created successfully, perform further operations
+  // })
+  // .catch((error) => {
+  //   console.error(error);
+  //   // Failed to open or create the database, handle the error
+  // });
+
   return (
   <AuthNavigator/>
   );
